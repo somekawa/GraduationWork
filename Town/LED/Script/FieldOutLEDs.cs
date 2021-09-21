e911b5a79ff8abd93e8ff6f4e26c63215c2ca194
saki <aaa>
2021-09-21 19:26:21 +0900

    Townシーン時にサブカメラ切り替わり時のみ点灯に変更

--- Town/LED/Script/FieldOutLEDs.cs
+++ Town/LED/Script/FieldOutLEDs.cs
@@ -1,12 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FieldOutLEDs : MonoBehaviour
 {
+    private GameObject cameraCtl_;      // CameraMng.csがアタッチされてるオブジェクト
+    private CameraMng cameraMng;        // メインカメラとサブカメラの切り替え
+
     private float intensity_ = 0.1f;
     private Renderer[] childrenRend_;
 
     void Start()
     {
+        if (SceneManager.GetActiveScene().name == "Town")
+        {
+            // オブジェクト経由でCameraMngScriptを取得
+            cameraCtl_ = GameObject.Find("CameraController");
+            cameraMng = cameraCtl_.transform.GetComponent<CameraMng>();
+        }
+
         childrenRend_ = new Renderer[transform.childCount];
         for (int i=0;i<transform.childCount;i++)
         {
@@ -17,9 +28,21 @@ public class FieldOutLEDs : MonoBehaviour
 
     void Update()
     {
-        intensity_ = Mathf.Sin(Time.time );// -1～1を取得
-        intensity_=Mathf.Abs(intensity_);// -1は暗くなるだけのため絶対値で正の値に変更
-        UpdateColor();
+        if (SceneManager.GetActiveScene().name != "Town")
+        {
+            intensity_ = Mathf.Sin(Time.time);// -1～1を取得
+            intensity_ = Mathf.Abs(intensity_);// -1は暗くなるだけのため絶対値で正の値に変更
+            UpdateColor();
+        }
+        else
+        {
+            if (cameraMng.subCamera.activeSelf == true)
+            {
+                intensity_ = Mathf.Sin(Time.time);// -1～1を取得
+                intensity_ = Mathf.Abs(intensity_);// -1は暗くなるだけのため絶対値で正の値に変更
+                UpdateColor();
+            }
+        }
     }
 
     private void UpdateColor()
