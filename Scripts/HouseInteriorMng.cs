eab9790d1d271f1a13914530bbc00b79618b73a4
somekawa <1816044@s.asojuku.ac.jp>
2021-09-26 15:27:29 +0900

    ①建物接触時に、選択中のコマンドの方に矢印アイコンを表示するようにした。 ②建物接触時に、建物名を表示するようにした。その際に、文字フォントが足りなかったため、新規フォントを追加しました。 ③「いいえ」選択時のスペースキー押下でワープ先選択処理が同時に発生してしまうバグを修正
    一時プレハブファイルに、[HouseInterior]の子にする必要がある[Canvas]を入れています。

--- Scripts/HouseInteriorMng.cs
+++ Scripts/HouseInteriorMng.cs
@@ -1,36 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HouseInteriorMng : MonoBehaviour
 {
+    public Canvas warpCanvas;       //  ワープ先を示すキャンバス(表示/非表示を切り替えるために使用)
+    public WarpTown warpTown;
+
     private CameraMng cameraMng_;
     private UnitychanController playerController_;
 
     private GameObject inHouseInfoCanvas_;
+    private GameObject iconImage_;
+    private TMPro.TextMeshProUGUI text_;
+
     private bool inHouseFlg_ = true;        // 入室するか(true:入る , false:入らない)
+
+    private readonly string[] buildNameEng_ = { "UniHouse","MayorHouse","BookStore","ItemStore", "Guild" , "Restaurant" };  // 建物名(ヒエラルキーと同じ英語)
+    private readonly string[] buildNameJpn_ = { "ユニの家", "町長の家" , "書店"    , "魔道具屋", "ギルド", "レストラン" };  // 建物名(表示用日本語)
+    private Dictionary<string, string> buildNameMap_ = new Dictionary<string, string>();    // キー:英語建物名,値:日本語建物名
 
     void Start()
     {
         cameraMng_ = GameObject.Find("CameraController").GetComponent<CameraMng>();
-        if (cameraMng_ == null)
-        {
-            Debug.Log("HouseInteriorMng.csで取得しているCameraMngがnullです");
-        }
-
         playerController_ = GameObject.Find("Uni").GetComponent<UnitychanController>();
-        if (playerController_ == null)
-        {
-            Debug.Log("HouseInteriorMng.csで取得しているplayerController_がnullです");
-        }
 
         inHouseInfoCanvas_ = this.transform.Find("Canvas").gameObject;
+        iconImage_ = inHouseInfoCanvas_.transform.Find("Icon").gameObject;
+        text_ = inHouseInfoCanvas_.transform.Find("HouseInfo/Text").GetComponent<TMPro.TextMeshProUGUI>();
+
+        // 英語建物名と日本語建物名を組み合わせる
+        for (int i = 0; i < buildNameEng_.Length; i++)
+        {
+            buildNameMap_.Add(buildNameEng_[i], buildNameJpn_[i]);
+        }
     }
 
     public bool SetHouseVisible(string name)
     {
         if (!inHouseFlg_)
         {
+            // 選択肢を「はい」に戻す
+            inHouseFlg_ = true;
+            // アイコン位置を「はい」に戻す
+            iconImage_.transform.localPosition = new Vector3(-140.0f, -70.0f, 0.0f);
+
+            // ワープ先キャンバスの表示
+            warpCanvas.gameObject.SetActive(true);
+
+            // ワープ処理を可能にするためにtrueに戻す
+            warpTown.enabled = true;
+
             // 入室処理時に必ずこの関数は呼ばれるが、
             // inHouseFlg_がfalseなら必要な処理をしてreturnする
             SetActiveCanvas(false, "");
@@ -74,11 +95,18 @@ public class HouseInteriorMng : MonoBehaviour
     // コルーチン  
     private IEnumerator SelectInHouse(bool flag,string name)
     {
+        // ワープ先キャンバスの非表示
+        warpCanvas.gameObject.SetActive(false);
+        // ワープ処理のスペースキーを止めるためにfalse
+        warpTown.enabled = false;
+
         // キャラアニメーションを止める
         playerController_.StopUniRunAnim();
 
         // キャラ操作を止める
         playerController_.enabled = false;
+
+        text_.text = buildNameMap_[name] + "に入る？";
 
         // キャンバスが表示中の間はコルーチン処理を行う
         while (flag)
@@ -87,11 +115,13 @@ public class HouseInteriorMng : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.RightArrow))
             {
+                iconImage_.transform.localPosition = new Vector3(40.0f, -70.0f, 0.0f);
                 inHouseFlg_ = false;
                 Debug.Log("選択肢「いいえ」");
             }
             else if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
+                iconImage_.transform.localPosition = new Vector3(-140.0f, -70.0f, 0.0f);
                 inHouseFlg_ = true;
                 Debug.Log("選択肢「はい」");
             }
